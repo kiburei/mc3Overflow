57b27b77812f48a7ef9338f786a5572c87ad2fb7
kiburei <akiburei@gmail.com>
2017-06-13 16:00:05 +0300

    add form for new question and accept params in component js

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        newQue(){
+            var params = {
+                question: this.get('question'),
+                author: this.get('author'),
+                date: this.get('date'), 
+            };
+            alert(params.question);
+        }
+    }
 });
