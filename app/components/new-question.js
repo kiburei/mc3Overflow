75eeeb1e051a8aee4f7798e83fe2ea40219fda32
kiburei <akiburei@gmail.com>
2017-06-13 17:38:08 +0300

    adding new dynamic route for question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -4,14 +4,16 @@ export default Ember.Component.extend({
     actions: {
         newQue(){
             var params = {
+                subject: this.get('subject'),
                 question: this.get('question'),
                 author: this.get('author'),
                 date: this.get('date'), 
             };
             this.sendAction('newQue1', params);
+            this.set('subject', '');
             this.set('question', '');
             this.set('author', '');
-            this.set('date', '');
+            this.set('dateg', '');
         }
     }
 });
