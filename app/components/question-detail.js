fb29fb14ba343b762e0b4e81c76295baebe83c7b
kiburei <akiburei@gmail.com>
2017-06-14 09:26:30 +0300

    add destroy to question component

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    editForm: false,
+    actions: {
+        editForm() {
+            this.set('editForm', true);
+        },
+        editQue(question) {
+         var params = {
+             question: this.get('question'),
+         };
+         this.set('question', '');
+         this.set('editForm', false);
+         this.sendAction('editQue1', question,  params);
+        },
+        destroy(question) {
+            if(confirm('Delete this question?')){
+                this.sendAction('destroy1', question);
+            }
+        }
+    }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.store.findRecord('question', params.question_id);
+    },
+    actions: {
+        editQue2(question, params) {
+            Object.keys(params).forEach(function(key) {
+    if(params[key]!==undefined) {
+      question.set(key,params[key]);
+    }
+  });
+            question.save();
+            this.transtionTo('index');
+        },
+        destroy2(question) {
+            question.destroyRecord();
+            this.transtionTo('index');
+        }
     }
 });
