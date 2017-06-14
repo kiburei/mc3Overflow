b0cb8552c17684d25c15d4f36b7bf72f9648bf23
kiburei <akiburei@gmail.com>
2017-06-14 09:42:05 +0300

    add model realtionship between question and answer

--- app/models/question.js
+++ app/models/question.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   author: DS.attr(),
   date: DS.attr(),
   subject: DS.attr(),
-  question: DS.attr()
+  question: DS.attr(),
+  answer: DS.hasMany('answer', {async: true})
 
 });
